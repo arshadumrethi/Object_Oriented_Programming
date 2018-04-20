df80fb4f2a195902e01c9c7b3aa8e704912a691c
arshadumrethi <arshad.umrethi@gmail.com>
2018-04-19 21:24:31 -0400

    Add new file

---
+++ oop_new6.py
@@ -0,0 +1,24 @@
+#If a subclass or derived class has been overwritten you can still directly
+#access the attributes or methods of the superclass using Pythons built-in Super function. 
+
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
+
+#Using Super to overwrite a parent method
+  def full_time_wage(self, hours):
+    return super(PartTimeEmployee, self).calculate_wage(hours)
+
+milton = PartTimeEmployee('Milton')
+print milton.full_time_wage(10)
